570abf12f69ab692ac01bc70af53b9b452947278
martialvaro088 <ams0003@alu.medac.es>
2024-01-21 16:26:26 +0100

    nuevos cambios

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,28 +1,68 @@
-var socket = io.connect('localhost:8080', { 'force': true });
-socket.on('messages', function (data) {
-    console.log(data);
-    render(data);
-})
-
-socket.on('hay-nuevo', function (data) {
-    alert(data);
-})
-
-function addMessage(e) {
-    var message = {
-        autor: document.getElementById("username").value,
-        texto: document.getElementById("texto").value
-    }
-    socket.emit("new-message", message);
-    return false;
+function irARegister() {
+    window.location.href = "register.html";
 }
 
-function render(data) {
-    var html = data.map(function (elem, index) {
-        return (`<div>
-                    <strong>${elem.autor}</strong>:
-                    <em>${elem.texto}</em>
-                </div>`);
-    }).join(" ");
-    document.getElementById('chat').innerHTML = html;
+function iniciarSesion() {
+
+    var usuario = document.getElementById('usuario').value;
+    var contraseña = document.getElementById('clave').value;
+
+    //HAGO LA PETICION AL SERVIDOR Y GUARDO LA RESPUESTA EN LA VARIABLE PROMISE
+    var promise = $.ajax({
+        type: 'POST',
+        url: '/identificar',
+        data: JSON.stringify({ username: usuario, password: contraseña }),
+        contentType: 'application/json;charset=UTF-8',
+        dataType: 'json'
+    });
+
+    //TRATAR LA RESPUESTA QUE ME DA EL SERVIDOR
+    promise.always(function (data) {
+
+        if (data.res == "login true") {
+            window.location.replace("/rutaSegura");
+        }
+        else if (data.res == "usuario no valido") {
+            alert("No estas autorizado, ese usuario no es valido");
+        }
+        else if (data.res == "login failed") {
+            alert("Debes introducir el usuario y contraseña");
+        }
+        else {
+            window.alert("Error")
+        }
+    })
+}
+
+function registrarUsuario() {
+
+    var usuario = document.getElementById('usuario').value;
+    var contraseña = document.getElementById('clave').value;
+
+    //HAGO LA PETICION AL SERVIDOR Y GUARDO LA RESPUESTA EN LA VARIABLE PROMISE
+    var promise = $.ajax({
+        type: 'POST',
+        url: '/registrar',
+        data: JSON.stringify({ username: usuario, password: contraseña }),
+        contentType: 'application/json;charset=UTF-8',
+        dataType: 'json'
+    });
+
+
+    //TRATAR LA RESPUESTA QUE ME DA EL SERVIDOR
+    promise.always(function (data) {
+
+        if (data.res == "register true") {
+            window.location.replace("/index.html");
+        }
+        else if (data.res == "usuario ya existe") {
+            alert("Ya existe ese usuario");
+        }
+        else if (data.res == "register failed") {
+            alert("Debes introducir el usuario y contraseña");
+        }
+        else {
+            window.alert("Error")
+        }
+    })
 }
